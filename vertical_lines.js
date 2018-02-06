dd22aac2d4622429d378261e058a5f44ec3259c8
ansariomar <omar.ansari@cca.edu>
2018-02-05 22:14:43 -0800

    Update vertical_lines.js

--- vertical_lines.js
+++ vertical_lines.js
@@ -1,6 +1,13 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw(){
+
 var counter = 10;
 while (counter <=width-10){
   line (counter,10,counter,330)
   counter = counter+5
 
+}
 }
