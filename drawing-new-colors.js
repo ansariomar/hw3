863c0c2fc33535ac3719ccbbd2ad2768af01e57e
ansariomar <omar.ansari@cca.edu>
2018-02-05 22:05:11 -0800

    Update drawing-new-colors.js

--- drawing-new-colors.js
+++ drawing-new-colors.js
@@ -14,18 +14,23 @@ function keyPressed() {
   print(key);
   if (key == 'M') {
     fill(255,0,255);
-  } else if (key == 'T') {
+  } else if (key == 'T') {//teal
     fill(0,128,128);
-  } else if (key == 'Y') {
+  } else if (key == 'Y') {//yellow
     fill(255,255,0); 
-  } else if (key == 'N') {
+  } else if (key == 'N') {//navy
     fill(0,0,128); 
-  } else if (key == 'B') {
+  } else if (key == 'B') {//beige
     fill(245,245,220); 
-  } else if (key == 'P') {
+  } else if (key == 'P') {//pink
     fill(255,105,180);
-  } else if (key == 'O') {
+  } else if (key == 'O') {//orange
     fill(255,165,0);
+  } 
+  
+  else if (key == '1') {
+    diameter (10)
 
 }
 }
+
