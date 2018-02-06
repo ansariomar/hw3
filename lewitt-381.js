ed058a4c0c62c1bc4991a06d5f0d0b07fec7ec98
ansariomar <omar.ansari@cca.edu>
2018-02-06 15:55:30 -0800

    Create lewitt-381.js

---
+++ lewitt-381.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+  fill(250);
+  rect(0, 0, 399, 399);
+  noStroke();
+
+  fill(178, 174, 167); //gray
+  rect(35, 35, 165, 165); //top L 
+  fill(255, 236, 113); //yellow
+  rect(200, 35, 165, 165); //top R 
+  fill(216, 67, 105); //pink
+  rect(35, 200, 165, 165); //bottom L 
+  fill(3, 162, 202); //blue
+  rect(200, 200, 165, 165); //bottom R 
+}
+
+
+// https://www.artsy.net/artwork/sol-lewitt-wall-drawing-number-381
