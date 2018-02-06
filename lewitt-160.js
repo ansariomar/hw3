4defff4aad99abf2fbce09bb3c57d5134ec4c0dc
ansariomar <omar.ansari@cca.edu>
2018-02-06 14:29:19 -0800

    Create lewitt-160.js

---
+++ lewitt-160.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+  strokeWeight(2);
+  fill(231,225,227)
+  rect(10, 10, 380, 380);
+
+  stroke(149,14,21)
+  line (50,50,350,350)
+  line (350,50,50,350)
+
+}
