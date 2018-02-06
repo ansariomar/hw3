9a14d7248552ef4047667e97b05c226b839bf64d
ansariomar <omar.ansari@cca.edu>
2018-02-06 14:38:09 -0800

    Update lewitt-160.js

--- lewitt-160.js
+++ lewitt-160.js
@@ -1,11 +1,17 @@
 function setup() {
   createCanvas(400, 400);
-  strokeWeight(2);
-  fill(231,225,227)
-  rect(10, 10, 380, 380);
+  strokeWeight(1);
+  fill(231, 225, 227)
+  rect(1, 1, 398, 398);
+  strokeWeight(1);
 
-  stroke(149,14,21)
-  line (50,50,350,350)
-  line (350,50,50,350)
+  rect(25, 25, 350, 350);
+
+
+  stroke(149, 14, 21)
+  line(60, 60, 335, 335)
+  line(335, 60, 60, 335)
 
 }
+
+\\http://massmoca.org/wp-content/uploads/2015/12/detail-page-half-width-160.jpg
