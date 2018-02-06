0fdc0bbb5df29d30c767ab3802886f943470c9ca
ansariomar <omar.ansari@cca.edu>
2018-02-05 17:24:23 -0800

    Create class_quilt.js

---
+++ class_quilt.js
@@ -0,0 +1,22 @@
+function yourPatch(x, y) {
+  noFill();
+  stroke(random(0,255),random(0,255),random(0,255));
+  rect(x, y, 100, 100);
+  
+function drawLine(){
+  line(random(x,x+100), random(y,y+100), (x+50), (y+50));
+}
+
+drawLine()
+  drawLine()
+var steps = 0
+while (steps < 100) {
+drawLine()
+steps = steps + 1
+}
+}
+
+background(255);
+
+yourPatch(0, 0);
+yourPatch(width-100, height-100);
