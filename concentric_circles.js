e3fe38c3529885a8b67d071c380180b0fec0a2a1
ansariomar <omar.ansari@cca.edu>
2018-02-05 22:23:07 -0800

    Update concentric_circles.js

--- concentric_circles.js
+++ concentric_circles.js
@@ -1,7 +1,14 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw(){
+
 var counter = 10;
 while (counter < 320){
   noFill();
   ellipse (170,170,counter,counter)//starts first line at x=10, then checks counter position, draws line 330px, counts 5 px, redraws
   counter = counter+5
 
+}
 }
