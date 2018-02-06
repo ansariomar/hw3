84a286c4e2b02ca9995c61373aff92365c9bd9cb
ansariomar <omar.ansari@cca.edu>
2018-02-05 23:22:26 -0800

    Update circle-grid.js

--- circle-grid.js
+++ circle-grid.js
@@ -1,22 +1,19 @@
 function setup() { 
-  createCanvas(400, 400);
-} 
-
+  createCanvas(400, 400); 
+  }
 function draw() { 
-  background(0);
-
-
-var x = 20;
-var y = 20;
-
-for (var x = 20; x<=380; x=x+20)
-{
-  for (y = 20; y<=380; y+= 20)
-{
-noStroke();
-fill(255);
-ellipse (x,y,15,15);
-
-}
-}
+  background(0); 
+  var x = 20;
+  var y = 20; 
+  
+  while (x<=380) 
+  {y=20; 
+  while (y<=380) 
+  {
+  noStroke(); 
+  fill(255); 
+  ellipse (x,y, 15,15); 
+  y+= 20} 
+  x+=20} 
+  
 }
