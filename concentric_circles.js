7f57376321e05dbfa0d65dff0661b61e791ec318
ansariomar <omar.ansari@cca.edu>
2018-02-05 17:15:31 -0800

    Create concentric_circles.js

---
+++ concentric_circles.js
@@ -0,0 +1,7 @@
+var counter = 10;
+while (counter < 320){
+  noFill();
+  ellipse (170,170,counter,counter)//starts first line at x=10, then checks counter position, draws line 330px, counts 5 px, redraws
+  counter = counter+5
+
+}
