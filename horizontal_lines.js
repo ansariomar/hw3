5fc0d10d2535db41d575bd5273a215a245f85639
ansariomar <omar.ansari@cca.edu>
2018-02-05 22:22:20 -0800

    Update horizontal_lines.js

--- horizontal_lines.js
+++ horizontal_lines.js
@@ -1,6 +1,12 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw(){
 var counter = 10;
 while (counter <=width-10){
   line (10,counter,330,counter)//starts first line at x=10, then checks counter position, draws line 330px, counts 5 px, redraws
   counter = counter+5
 
+}
 }
