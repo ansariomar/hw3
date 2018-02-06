ad3ec95f0b06e1907c79df45c036f3d08e51e64a
ansariomar <omar.ansari@cca.edu>
2018-02-06 14:04:43 -0800

    Create drawing-with-diameter.js

---
+++ drawing-with-diameter.js
@@ -0,0 +1,41 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+var diameter = 10;
+
+function draw() {
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'M') {
+    fill(255, 0, 255);
+  } else if (key == 'T') { //teal
+    fill(0, 128, 128);
+  } else if (key == 'Y') { //yellow
+    fill(255, 255, 0);
+  } else if (key == 'N') { //navy
+    fill(0, 0, 128);
+  } else if (key == 'B') { //beige
+    fill(245, 245, 220);
+  } else if (key == 'P') { //pink
+    fill(255, 105, 180);
+  } else if (key == 'O') { //orange
+    fill(255, 165, 0);
+  }
+
+  if (key == '1') {
+    diameter = 100
+}
+  
+  else if (key =='2') {
+    diameter = 200
+  }
+  else if (key =='3'){
+    diameter = 300
+}
+}
